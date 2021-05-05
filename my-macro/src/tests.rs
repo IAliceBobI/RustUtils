0a6dc978a010e41caa648e384b5ee0c5f538a68c
chenwei <chenwei767@gmail.com>
2021-05-05 21:36:39 +0800

    Add util for hashmap

--- my-macro/src/tests.rs
+++ my-macro/src/tests.rs
@@ -4,6 +4,16 @@ use super::*;
 
 #[test]
 fn init_array() {
-	let strings = init_array![String::from("hi!"); 200];
-	assert_eq!(strings.len(), 200);
+	let strings = init_array![String::from("hi!"); 20];
+	assert_eq!(strings.len(), 20);
+}
+
+#[test]
+fn init_hashmap() {
+	let m = init_hashmap!(
+		"a", 3,
+		"b", 4,
+		"c", 5,
+	);
+	assert_eq!(m.len(), 3);
 }
